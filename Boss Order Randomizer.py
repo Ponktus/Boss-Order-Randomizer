7a14197083e4f658cb257150b1b7969552a78e3c
Ponktus <37674591+Ponktus@users.noreply.github.com>
2018-10-13 23:37:55 +0200

    Cleaned up the code and added base for the progression item logic.

--- Boss Order Randomizer.py
+++ Boss Order Randomizer.py
@@ -1,5 +1,4 @@
 import random, struct, json, time
-import numpy as np
 
 ### Picks a random boss order and returns it as a list.
 
@@ -20,34 +19,16 @@ def byte_splitter(Index, Visibility):
 	Item_Code = Item_Properties[Index]['Code']
 	Bytes[1] = int(Item_Code[:-2], 16)
 	Bytes[0] = int(Item_Code[2:] + Item_Code[:2], 16)
-	if Visibility = Chozo:
+	if Visibility == 'Chozo':
 		Bytes[0] += 0x54
-	elif Visibility = Hidden:
+	elif Visibility == 'Hidden':
 		Bytes[0] += 0xA8
 	return Bytes
 
-def major_items_distribution():
-	List_Of_Majors = ['Bombs', 'Springball', 'Varia', 'Gravity', 'Hijump', 'Speed', 'Spacejump', 'Screw', 'Charge', 'Spazer', 'Wave', 'Ice', 'Plasma', 'Grapple', 'Xray']
-	Distribution = {}
-	Limitations = {}
-	for i in range(10):
-		Distribution[str(Item_Config[i]['Name'])] = ""
-		Limitations[str(Item_Config[i]['Name'])] = ""
-	Item = pick_random_major(List_Of_Majors)
-	List_Of_Majors.remove(Item)
-	Distribution[pick_random_location(Distribution, Limitations, Item)] = Item
+### Picks a random object from a list. Used to pick random item locations and random items from lists.
 
-#def pick_random_location(Locations, Limitations, Item):
-
-#def Location_Possible(Distribution, Item, Target):
-
-def pick_random_major(Major_List):
-	return Major_List[random.randint(0, len(Major_List))]
-
-def ByteToHex(bytestring):
-	return ''.join( [ "%02X " % ord( x ) for x in bytestring ] ).strip()
-
-
+def pick_random_list_object(list):
+	return list[random.randint(0, len(list))]
 
 print('Initializing...')
 
@@ -63,8 +44,6 @@ Json = open('Items.json', 'r')
 Item_Properties = json.load(Json)
 Json.close
 
-toHex = lambda x:"".join([hex(ord(c))[2:].zfill(2) for c in x])
-
 Rom_File = open("SuperMetroid.sfc", "r+")
 Rom_File.seek(0x786DE+2)
 print(ByteToHex(Rom_File.read(2)))
@@ -73,6 +52,33 @@ Rom_File.write(chr(0x73))
 Rom_File.write(chr(0xEF))
 Rom_File.seek(0x786DE+2)
 print(ByteToHex(Rom_File.read(2)))
+
+### Setting up information and item locks based on boss order and config file.
+
+Locked_Items = Item_Locations.copy()
+Unlocked_Items = []
+
+Boss_Order = boss_order()
+Ridley_Position = Boss_Order.index('Ridley')
+Draygon_Position = Boss_Order.index('Draygon')
+Phantoon_Position = Boss_Order.index('Phantoon')
+Kraid_Position = Boss_Order.index('Kraid')
+
+for i in range(4):
+	for j in range(4-(i+1)):
+		for k in range(100):
+			if Locked_Items[k]['BossLock'] == Boss_Order[j]:
+				#Need to add Boss_Requirements.json for comparison and to add prerequisites
+
+for x in range(16):
+	for i in range(len(Locked_Items)):
+		if not Item_Locations[i]['ItemLock'] and not Item_Locations[i]['ItemSetLock']:
+			Unlocked_Items.append(Item_Locations[i])
+			del Locked_Items[i]
+	# Make List of all progress locking items
+	# Pick a random of these items and put it on a slot from the Unlocked_Items list.
+	# Cross out Itemlocks on the Locked_Items list
+
 
 #for i in range(100):
 #	Rom_File.seek(int(Item_Locations[i]["Adress"], 16))
