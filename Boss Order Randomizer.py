d3c1d53ac97ccc761784bfe844fb5c852f6043c4
Ponktus <37674591+Ponktus@users.noreply.github.com>
2018-10-15 20:59:38 +0200

    Developed the logic further and fixed some bugs.

--- Boss Order Randomizer.py
+++ Boss Order Randomizer.py
@@ -27,37 +27,43 @@ def byte_splitter(Index, Visibility):
 
 ### Picks a random object from a list. Used to pick random item locations and random items from lists.
 
-def pick_random_list_object(list):
-	return list[random.randint(0, len(list))]
+def pick_random_list_object(list_object):
+	return random.choice(list_object)
 
 print('Initializing...')
 
-### Opens Item_Locations.json and stores it in an array 'Item_Locations' for later use.
+### Opens Item_Locations.json, Items.json, Item_Set_Config.json and stores them in an array for later use.
 
 Json = open('Item_Locations.json', 'r')
-Item_Locations = json.load(Json)
+Locked_Items = json.load(Json)
+Item_Locations = list(Locked_Items)
 Json.close
-
-### Opens Items.json and stores it in an array 'Item_Properties' for later use.
 
 Json = open('Items.json', 'r')
 Item_Properties = json.load(Json)
 Json.close
 
-Rom_File = open("SuperMetroid.sfc", "r+")
-Rom_File.seek(0x786DE+2)
-print(ByteToHex(Rom_File.read(2)))
-Rom_File.seek(0x786DE)
-Rom_File.write(chr(0x73))
-Rom_File.write(chr(0xEF))
-Rom_File.seek(0x786DE+2)
-print(ByteToHex(Rom_File.read(2)))
+Json = open('Item_Set_Config.json', 'r')
+Item_Lock_Sets = json.load(Json)
+Json.close
+
+### Rom write test
+
+#Rom_File = open("SuperMetroid.sfc", "r+")
+#Rom_File.seek(0x786DE+2)
+#Rom_File.seek(0x786DE)
+#Rom_File.write(chr(0x73))
+#Rom_File.write(chr(0xEF))
+#Rom_File.seek(0x786DE+2)
 
 ### Setting up information and item locks based on boss order and config file.
 
-Locked_Items = Item_Locations.copy()
+High_Tier_Items = ["Morph", "Supers", "Powerbombs", "Speed", "Varia", "Gravity"]
 Unlocked_Items = []
-
+Item_Distribution = []
+for i in range(100):
+	Item_Distribution.append([Item_Locations[i]['Adress']])
+Progress_Items_Distributed = False
 Boss_Order = boss_order()
 Ridley_Position = Boss_Order.index('Ridley')
 Draygon_Position = Boss_Order.index('Draygon')
@@ -70,10 +76,10 @@ Kraid_Position = Boss_Order.index('Kraid')
 #			if Locked_Items[k]['BossLock'] == Boss_Order[j]:
 				#Need to add Boss_Requirements.json for comparison and to add prerequisites
 
-for x in range(16):
-    for i in range(len(Locked_Items)):
+while not Progress_Items_Distributed:
+    for i in range(len(Item_Locations)):
         if not Item_Locations[i]['ItemLock'] and not Item_Locations[i]['ItemSetLock']:
-            Unlocked_Items.append(Item_Locations[i])
+            Unlocked_Items.append(Item_Locations[i]['Adress'])
             del Locked_Items[i]
     # Make List of all progress locking items
     Progress_Items = []
@@ -86,17 +92,23 @@ for x in range(16):
             for j in range(len(Locked_Items[i]['ItemSetLock'])):
                 if not Locked_Items[i]['ItemSetLock'][j] in Progress_Items:
                     Progress_Items.append(Locked_Items[i]['ItemSetLock'][j])
-        
-	# Pick a random of these items and put it on a slot from the Unlocked_Items list.
+	# Pick a random of these items and put it on a slot from the Unlocked_Items list into the Item_Distribution list.
+    Next_Item = pick_random_list_object(Progress_Items) 						#Name as string
+    Progress_Items = []
+    #Checking the easier case first. Next_Item is an item and not an itemset
+    if Next_Item in High_Tier_Items:
+    	for i in range(len(Locked_Items)):
+			if Next_Item in Locked_Items[i]['ItemLock']:
+				Locked_Items[i]['ItemLock'].remove(Next_Item)
+
+
+
+
+
+    	High_Tier_Items.remove(Next_Item)
+    	Location = pick_random_list_object(Unlocked_Items)							#Adress as hex
+    	Item_Distribution.append([Location, Next_Item])
+
+
+
 	# Cross out Itemlocks on the Locked_Items list
-
-
-#for i in range(100):
-#	Rom_File.seek(int(Item_Locations[i]["Adress"], 16))
-#	Index = 0
-#	for j in range(21):
-#		if Item_Distribution[i][1] = Item_Properties[j]['Name']:
-#			Index = j
-#	Bytes = byte_splitter(Index, Visibility)
-
-
